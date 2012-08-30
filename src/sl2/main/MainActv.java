a6a397f5a04ff75430af5b0c1a06961c53c7a389
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-08-30 13:43:34 +0900

    m
    1.0
    
    ---
    1. Up to => Run the project
    	==> "R.java" generated

---
+++ src/sl2/main/MainActv.java
@@ -0,0 +1,13 @@
+package sl2.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActv extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
