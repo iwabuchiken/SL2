b259747f9de119478dbe36ed3955e90386654047
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-08-30 14:00:08 +0900

    m
    1.5
    
    ---
    1. Up to => main.xml ==> Shown

--- src/sl2/main/MainActv.java
+++ src/sl2/main/MainActv.java
@@ -1,13 +1,18 @@
 package sl2.main;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MainActv extends Activity {
+public class MainActv extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-}
+        
+        setTitle(this.getClass().getName());
+        
+    }//public void onCreate(Bundle savedInstanceState)
+    
+}//public class MainActv extends ListActivity
